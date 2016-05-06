33da21777b69fd88d1bbb74b64b87efbdf4620ef
Iuri Rezende Souza <lrezende@usc.edu>
2016-05-05 18:43:55 -0700

    📝 Scratch a functional widget class

--- test.py
+++ test.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 
 from PyQt5 import QtWidgets
+import hashlib
+
+class SignedTextEdit(QtWidgets.QWidget):
+    '''Widget for editing a text while displaying some checksums'''
+    def __init__(self, initial_text=''):
+        '''Creates the widget with optionally an initial text'''
+        QtWidgets.QWidget.__init__(self)
+        self.my_layout = QtWidgets.QVBoxLayout()
+        self.setLayout(self.my_layout)
+        self.my_text = QtWidgets.QTextEdit(initial_text)
+        self.my_layout.addWidget(self.my_text)
+        self.sha1label = QtWidgets.QLabel(self._sha1_string())
+        self.my_layout.addWidget(self.sha1label)
+        self.my_text.textChanged.connect(self._update_label)
+    def _sha1_string(self):
+        '''Computes the string to be displayed on the label'''
+        text = self.my_text.toPlainText()
+        return 'SHA1: '+hashlib.sha1(text.encode()).hexdigest()
+    def _update_label(self):
+        '''Slot to update the label'''
+        self.sha1label.setText(self._sha1_string())
 
 
 class button_printer_ender(QtWidgets.QPushButton):
@@ -19,7 +40,7 @@ if __name__ == '__main__':
     main_win = QtWidgets.QWidget()
     org = QtWidgets.QVBoxLayout()
     hello = QtWidgets.QLabel('Type some text and click the button')
-    text = QtWidgets.QTextEdit()
+    text = SignedTextEdit()
     button = button_printer_ender(app, text)
     org.addWidget(hello)
     org.addWidget(text)
